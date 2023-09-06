5c2110462d3ca897a42595b5d85274d0c42ba4d2
amirbar002 <amirbar002@gmail.com>
2023-09-06 19:28:46 +0300

    full web

---
+++ Angular-Full-Stack/README.md
@@ -0,0 +1,4 @@
+Build frontend: npm run builddev for dev or npm run build for prod
+Build backend: npm run predev
+Run MongoDB: mongod
+Run the app: npm start
---
+++ Angular-Full-Stack/server/controllers/base.ts
@@ -0,0 +1,70 @@
+import { Request, Response } from 'express';
+
+abstract class BaseCtrl {
+  abstract model: any;
+
+  getAll = async (req: Request, res: Response) => {
+    try {
+      const docs = await this.model.find({});
+      return res.status(200).json(docs);
+    } catch (err: any) {
+      return res.status(400).json({ error: err.message });
+    }
+  };
+
+  count = async (req: Request, res: Response) => {
+    try {
+      const count = await this.model.count();
+      return res.status(200).json(count);
+    } catch (err: any) {
+      return res.status(400).json({ error: err.message });
+    }
+  };
+
+  insert = async (req: Request, res: Response) => {
+    try {
+      const obj = await new this.model(req.body).save();
+      return res.status(201).json(obj);
+    } catch (err: any) {
+      return res.status(400).json({ error: err.message });
+    }
+  };
+
+  get = async (req: Request, res: Response) => {
+    try {
+      const obj = await this.model.findOne({ _id: req.params.id });
+      return res.status(200).json(obj);
+    } catch (err: any) {
+      return res.status(500).json({ error: err.message });
+    }
+  };
+
+  update = async (req: Request, res: Response) => {
+    try {
+      await this.model.findOneAndUpdate({ _id: req.params.id }, req.body);
+      return res.sendStatus(200);
+    } catch (err: any) {
+      return res.status(400).json({ error: err.message });
+    }
+  };
+
+  delete = async (req: Request, res: Response) => {
+    try {
+      await this.model.findOneAndRemove({ _id: req.params.id });
+      return res.sendStatus(200);
+    } catch (err: any) {
+      return res.status(400).json({ error: err.message });
+    }
+  };
+
+  deleteAll = async (_req: Request, res: Response) => {
+    try {
+      await this.model.deleteMany();
+      return res.sendStatus(200);
+    } catch (err: any) {
+      return res.status(400).json({ error: err.message });
+    }
+  };
+}
+
+export default BaseCtrl;
